e246a48239d539245664c16e8a3ce601394e677a
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2023-09-04 00:06:49 -0400

    Disable password muation on test account

--- src/features/authentication/UpdatePasswordForm.jsx
+++ src/features/authentication/UpdatePasswordForm.jsx
@@ -5,12 +5,19 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUpdateUser } from "./useUpdateUser";
+import { useUser } from "./useUser";
+import Spinner from "../../ui/Spinner";
 
 function UpdatePasswordForm() {
   const { register, handleSubmit, formState, getValues, reset } = useForm();
   const { errors } = formState;
 
   const { updateUser, isUpdating } = useUpdateUser();
+
+  // Disabling form for test user
+  const { user, isLoading } = useUser();
+  if (isLoading) return <Spinner />;
+  const isNotAllowed = user?.email === "testuser@generic.com";
 
   function onSubmit({ password }) {
     updateUser({ password }, { onSuccess: reset });
@@ -23,10 +30,13 @@ function UpdatePasswordForm() {
         error={errors?.password?.message}
       >
         <Input
-          type="password"
+          type={isNotAllowed ? "text" : "password"}
           id="password"
           autoComplete="current-password"
-          disabled={isUpdating}
+          disabled={isUpdating || isNotAllowed}
+          value={
+            isNotAllowed ? "You cannot change this account's password" : ""
+          }
           {...register("password", {
             required: "This field is required",
             minLength: {
@@ -42,10 +52,13 @@ function UpdatePasswordForm() {
         error={errors?.passwordConfirm?.message}
       >
         <Input
-          type="password"
+          type={isNotAllowed ? "text" : "password"}
           autoComplete="new-password"
           id="passwordConfirm"
-          disabled={isUpdating}
+          disabled={isUpdating || isNotAllowed}
+          value={
+            isNotAllowed ? "You cannot change this account's password" : ""
+          }
           {...register("passwordConfirm", {
             required: "This field is required",
             validate: (value) =>
@@ -57,7 +70,7 @@ function UpdatePasswordForm() {
         <Button onClick={reset} type="reset" variation="secondary">
           Cancel
         </Button>
-        <Button disabled={isUpdating}>Update password</Button>
+        <Button disabled={isUpdating || isNotAllowed}>Update password</Button>
       </FormRow>
     </Form>
   );
